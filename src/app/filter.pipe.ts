d5110b39d198c90e36f40fee6447f7847d879b1b
Łukasz Kochańczyk <lukaszek2302@gmail.com>
2023-05-21 20:55:48 +0200

    Changed logic of pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -7,21 +7,19 @@ import { JobOffer } from './job-offer/job-offer';
 })
 export class FilterPipe implements PipeTransform {
     transform(jobOffers: JobOffer[], searchTags: string[]) {
-        const filterArray: JobOffer[] = [];
-        if (searchTags.length === 0) {
-            return jobOffers;
-        } else {
-            for (const singleTag of searchTags) {
-                for (const jobOffer of jobOffers) {
-                    if (jobOffer.role === singleTag || jobOffer.level === singleTag || 
-                        jobOffer.languages.includes(singleTag) || jobOffer.tools.includes(singleTag)) {
-                        if (!filterArray.includes(jobOffer)) {
-                            filterArray.push(jobOffer);
-                        }
-                    }
-                }   
-            }
-            return filterArray;
-        }
+      if (searchTags.length === 0) {
+        return jobOffers;
+      } else {
+        return jobOffers.filter((jobOffer: JobOffer) => {
+          return searchTags.every((singleTag: string) => {
+            return (
+              jobOffer.role === singleTag ||
+              jobOffer.level === singleTag ||
+              jobOffer.languages.includes(singleTag) ||
+              jobOffer.tools.includes(singleTag)
+            );
+          });
+        });
+      }
     }
 }
